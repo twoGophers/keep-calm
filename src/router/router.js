bda3fe2a264abf162080a366b7de0fbff8250304
Vladimir <falinvlad93@rambler.ru>
2022-07-31 19:51:38 +0300

    Readmi file

--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter,  createWebHashHistory } from 'vue-router';
 
 import VLorem from "../pages/page/VLorem.vue";
 import VIpsum from "../pages/page/VIpsum.vue";
 
 export default createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         { path: '/', component : VLorem },
         { path: '/ipsum', component : VIpsum },
